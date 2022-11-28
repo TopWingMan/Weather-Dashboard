fc4f5a4b67e9fa744e8e913c6d0295f6c1ffd33c
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-27 17:47:58 -0800

    trying to fix api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 window.onload = function() 
 {
     const buttonArray = ["atlanta", "denver", "seattle", "san francisco", "orlando", "new york", "chicago", "austin"];
+    const apiKey = "820747b9100e2cc91a280dbc0b2581f6";
+    var cityName = "";
 
     for (var i = 0; i < buttonArray.length; i++)
     {
-        document.getElementById(buttonArray[i]).addEventListener('click', setButtonValue.bind(this, buttonArray[i]));
+        document.getElementById(buttonArray[i]).addEventListener('click', searchButtonValue.bind(this, buttonArray[i]));
     }
     
-    function setButtonValue(buttonName)
+    function searchButtonValue(buttonName)
     {
         console.log(buttonName);
+        //document.getElementById(searchInput).innerHTML = "CHeese";
     }
+
+    function FindACity(cityName)
+    {
+        
+    }
+
+    //api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey};
 }
