22a2cea58663d97fa8ad71c11b1b73258d6cf5e0
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-27 19:11:45 -0800

    Forecast text for temp, wind, and humdity can be set with functions now

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,66 @@ window.onload = function()
 
     function FindACity(cityName)
     {
-        
+        // fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=820747b9100e2cc91a280dbc0b2581f6')
+        // .then((response) => response.json())
+        // .then((data) => console.log(data));
+
+        //Find latitude
+        fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=820747b9100e2cc91a280dbc0b2581f6')
+        .then((response) => response.json())
+        .then((data) => console.log(data[0].lat));
+
+        //Find longitude
+        fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=820747b9100e2cc91a280dbc0b2581f6')
+        .then((response) => response.json())
+        .then((data) => console.log(data[0].lon));
+
+        //Find weather for place
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&appid=820747b9100e2cc91a280dbc0b2581f6")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+    FindACity();
+
+    //Set Information
+    function SetDates()
+    {
+        for (i = 1; i < 6; i++)
+        {
+            document.getElementById('date' + i).innerHTML = "9/14/2022 (var)";
+        }
     }
 
-    //api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey};
+    function SetTemps()
+    {
+        for (i = 1; i < 6; i++)
+        {
+            document.getElementById('temp' + i).innerHTML = "Temp: 63.55 F (var)";
+        }
+    }
+
+    function SetWinds()
+    {
+        for (i = 1; i < 6; i++)
+        {
+            document.getElementById('wind' + i).innerHTML = "Wind: 3.94 MPH (var)";
+        }
+    }
+
+    function SetHumids()
+    {
+        for (i = 1; i < 6; i++)
+        {
+            document.getElementById('humid' + i).innerHTML = "Humidity: 63% (var)";
+        }
+    }
+
+    function SetForecast()
+    {
+        SetDates();
+        SetTemps();
+        SetWinds();
+        SetHumids();
+    }
+    SetForecast();
 }
