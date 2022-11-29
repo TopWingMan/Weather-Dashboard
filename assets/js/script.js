6e45fb64f8c7c504c9afda9db3ce13918e3955b7
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-29 13:29:58 -0800

    todays weather and forecasts are now updated to their weather values

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,6 @@ window.onload = function()
 {
     const buttonArray = ["atlanta", "denver", "seattle", "san francisco", "orlando", "new york", "chicago", "austin"];
     const apiKey = "820747b9100e2cc91a280dbc0b2581f6";
-    var cityName = "";
-    var cityLat;
-    var cityLon;
 
     for (var i = 0; i < buttonArray.length; i++)
     {
@@ -17,100 +14,73 @@ window.onload = function()
         //document.getElementById(searchInput).innerHTML = "CHeese";
     }
 
-    function FindACity(cityName)
+    function SetWeatherToCity(cityName)
     {
         fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=820747b9100e2cc91a280dbc0b2581f6')
         .then((response) => response.json())
         .then((output) => 
         {
             //Find Latitude of city
-            cityLat = output[0].lat;
+            var cityLat = output[0].lat;
 
             //Find Longitude of city
-            cityLon = output[0].lon;
+            var cityLon = output[0].lon;
 
-            //Find weather for coordinates
-            fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=820747b9100e2cc91a280dbc0b2581f6")
-            .then((response) => response.json())
-            .then((output) =>
-            {
-                //Get date
-                var currentDay = (output.list[0].dt_txt.substring(8, 10)); //Get Day
-                var currentMonth = (output.list[0].dt_txt.substring(5, 7)); //Get Month
-                var currentYear = (output.list[0].dt_txt.substring(0, 4)); //Get Year
-                console.log("(" + currentMonth + "/" + currentDay + "/" + currentYear + ")");
-
-                //Get icon
-                console.log("Icon: " + output.list[0].weather[0].icon);
-
-                //Get tempature
-                console.log("Temp: " + (((output.list[0].main.temp_max - 273.15) * 1.8) + 32) + "F");
-
-                //Get wind
-                console.log("Wind: " + output.list[0].wind.speed + " MPH");
-
-                //Get humidity
-                console.log("Humidity: " + output.list[0].main.humidity + "%");
-            })
+            FindWeather(cityName, cityLat, cityLon, 0); //Find and set todays weather to citynames weather
+            FindWeather(cityName, cityLat, cityLon, 1); //Find and set forecast1 weather to citynames forecast1 weather
+            FindWeather(cityName, cityLat, cityLon, 9);
+            FindWeather(cityName, cityLat, cityLon, 17);
+            FindWeather(cityName, cityLat, cityLon, 25);
+            FindWeather(cityName, cityLat, cityLon, 33);
         })
     }
-    FindACity("troutdale");
+    SetWeatherToCity("Troutdale");
 
-    //Set Forecast Information
-    function SetDates()
+    function FindWeather(cityName ,cityLat, cityLon, value)
     {
-        for (i = 1; i < 6; i++)
+        //Find weather for coordinates
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=820747b9100e2cc91a280dbc0b2581f6")
+        .then((response) => response.json())
+        .then((output) =>
         {
-            document.getElementById('date' + i).innerHTML = "9/14/2022 (var)"; //actually set using i
-        }
-    }
+            //Get date
+            var day = (output.list[value].dt_txt.substring(8, 10)); //Get Day
+            var month = (output.list[value].dt_txt.substring(5, 7)); //Get Month
+            var year = (output.list[value].dt_txt.substring(0, 4)); //Get Year
+            var date = ("(" + month + "/" + day + "/" + year + ")");
 
-    function SetTemps()
-    {
-        for (i = 1; i < 6; i++)
-        {
-            document.getElementById('temp' + i).innerHTML = "Temp: 63.55 F (var)";
-        }
-    }
+            //Get icon
+            var icon = ("Icon: " + output.list[value].weather[0].icon);
 
-    function SetWinds()
-    {
-        for (i = 1; i < 6; i++)
-        {
-            document.getElementById('wind' + i).innerHTML = "Wind: 3.94 MPH (var)";
-        }
-    }
+            //Get tempature
+            var temp = ("Temp: " + (((output.list[value].main.temp_max - 273.15) * 1.8) + 32).toString().substring(0, 4) + "F");
 
-    function SetHumids()
-    {
-        for (i = 1; i < 6; i++)
-        {
-            document.getElementById('humid' + i).innerHTML = "Humidity: 63% (var)";
-        }
-    }
+            //Get wind
+            var wind = ("Wind: " + output.list[value].wind.speed + " MPH");
 
-    function SetForecasts()
-    {
-        SetDates();
-        SetTemps();
-        SetWinds();
-        SetHumids();
+            //Get humidity
+            var humidity = ("Humidity: " + output.list[value].main.humidity + "%");
+
+            SetForecast(cityName, date, icon, temp, wind, humidity, value);
+        })
     }
-    SetForecasts();
 
     //Set Today Box Information
-    function SetTodayBox()
+    function SetForecast(cityName, date, icon, temp, wind, humidity, value)
     {
-        document.getElementById("today-city-date").innerHTML = "Atlanta (9/13/2022) (var)";
-        document.getElementById("today-temp").innerHTML = "Temp: 76.62F (var)";
-        document.getElementById("today-wind").innerHTML = "Wind: 8.43 MPH (var)";
-        document.getElementById("today-humidity").innerHTML = "Humidity: 44% (var)";
-    }
-    SetTodayBox();
-
-
-    function getWeather()
-    {
-        
+        if (value === 0) //Update today Box
+        {
+            document.getElementById("today-city-date").innerHTML = cityName + " " + date;
+            document.getElementById("today-temp").innerHTML = temp;
+            document.getElementById("today-wind").innerHTML = wind;
+            document.getElementById("today-humidity").innerHTML = humidity;
+        }
+        else if (value > 0)
+        {
+            document.getElementById('date' + value).innerHTML = date;
+            document.getElementById('temp' + value).innerHTML = temp;
+            document.getElementById('wind' + value).innerHTML = wind;
+            document.getElementById('humid' + value).innerHTML = humidity;
+        }
     }
 }
